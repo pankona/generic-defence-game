da409b9acf4c616aaee90d2565a480a325915eb1
pankona <yosuke.akatsuka@gmail.com>
2023-10-11 23:39:22 +0900

    introduce wave

---
+++ wave.go
@@ -0,0 +1,49 @@
+package main
+
+type EnemySpawnInfo struct {
+	SpawnFrame int // 何フレーム後に敵をスポーンさせるか
+}
+
+type Wave struct {
+	EnemySpawns    []EnemySpawnInfo // このウェーブでの敵の出現情報
+	TotalFrames    int              // このウェーブの持続フレーム数（次のウェーブが開始するまでのフレーム数）
+	EnemiesSpawned int              // このウェーブでスポーンさせた敵の数
+}
+
+type Stage struct {
+	Waves []Wave // このステージにおける各ウェーブの情報
+}
+
+// サンプルの敵出現情報
+var sampleStage = Stage{
+	Waves: []Wave{
+		{
+			EnemySpawns: []EnemySpawnInfo{
+				{SpawnFrame: 60},  // 1秒後 (60fps 前提)
+				{SpawnFrame: 120}, // 2秒後
+				{SpawnFrame: 180}, // 3秒後
+			},
+			TotalFrames: 300, // 5秒間のウェーブ
+		},
+		{
+			EnemySpawns: []EnemySpawnInfo{
+				{SpawnFrame: 60},
+				{SpawnFrame: 90},
+				{SpawnFrame: 150},
+				{SpawnFrame: 210},
+			},
+			TotalFrames: 360, // 6秒間のウェーブ
+		},
+		{
+			EnemySpawns: []EnemySpawnInfo{
+				{SpawnFrame: 60},
+				{SpawnFrame: 90},
+				{SpawnFrame: 120},
+				{SpawnFrame: 150},
+				{SpawnFrame: 180},
+				{SpawnFrame: 210},
+			},
+			TotalFrames: 360, // 6秒間のウェーブ
+		},
+	},
+}
